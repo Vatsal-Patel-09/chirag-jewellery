85937abdc0a90d84ee86a5abfbe420a1a43e0ca7
Vatsal-Patel-09 <vatsalpatel952005@gmail.com>
2026-02-10 23:36:16 +0530

    Add Navbar and Footer to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,23 @@ import FeaturedProducts from "@/components/home/FeaturedProducts";
 import PromoBanner from "@/components/home/PromoBanner";
 import BlogPreview from "@/components/home/BlogPreview";
 import InstagramFeed from "@/components/home/InstagramFeed";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 export default function HomePage() {
   return (
     <>
-      <Hero />
-      <TrustBar />
-      <CategoryShowcase />
-      <FeaturedProducts />
-      <PromoBanner />
-      <BlogPreview />
-      <InstagramFeed />
+      <Navbar />
+      <main>
+        <Hero />
+        <TrustBar />
+        <CategoryShowcase />
+        <FeaturedProducts />
+        <PromoBanner />
+        <BlogPreview />
+        <InstagramFeed />
+      </main>
+      <Footer />
     </>
   );
 }
